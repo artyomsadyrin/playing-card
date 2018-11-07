9e960775da2e5d63c2b0a42a9479f4ccc1dd07fd
Artyom Sadyrin <artsiomsadyryn@gmail.com>
2018-11-07 17:14:49 +0300

    Implemented a model structs PlayingCard & PlayingCardDeck, added check if it works

--- playing-card/playing-card/PlayingCard.swift
+++ playing-card/playing-card/PlayingCard.swift
@@ -8,18 +8,59 @@
 
 import Foundation
 
-struct PlayingCard {
+struct PlayingCard: CustomStringConvertible {
+    var description: String {
+        return "\(suit), Rank: \(rank)"
+    }
+    
     var suit: Suit
     var rank: Rank
     
-    enum Suit: String {
+    enum Suit: String, CustomStringConvertible {
+        var description: String {
+            return "Suit: \(String(Substring(self.rawValue)))"
+        }
+        
         case spades = "♠️"
         case hearts = "♥️"
         case diamonds = "♦️"
         case clubs = "♣️"
+        
+        static var all: [Suit] = [.spades, .hearts, .diamonds, .clubs]
     }
     
     enum Rank {
-        case 
+        
+        
+        case ace
+        case face(String)
+        case numeric(Int)
+        
+        var order: Int {
+            switch self {
+            case .ace:
+                return 1
+            case .numeric(let pips):
+                return pips
+            case .face(let kind) where kind == "J":
+                return 11
+            case .face(let kind) where kind == "Q":
+                return 12
+            case .face(let kind) where kind == "K":
+                return 13
+            default:
+                return 0
+            }
+        }
+        
+        static var all: [Rank] {
+            var allRanks: [Rank] = [.ace]
+            for pips in 2...10 {
+                allRanks.append(.numeric(pips))
+            }
+            allRanks += [.face("J"), .face("Q"), .face("K")]
+            
+            return allRanks
+        }
     }
 }
