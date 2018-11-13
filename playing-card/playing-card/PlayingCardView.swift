0f30db7f504265b1b2dea55ce15285f14e6a440c
Artyom Sadyrin <artsiomsadyryn@gmail.com>
2018-11-13 10:23:38 +0300

    Made utility extension for CGRect & CGPoint

--- playing-card/playing-card/PlayingCardView.swift
+++ playing-card/playing-card/PlayingCardView.swift
@@ -87,3 +87,29 @@ extension PlayingCardView {
         }
     }
 }
+
+extension CGRect {
+    var leftHalf: CGRect {
+        return CGRect(x: minX, y: minY, width: width/2, height: height)
+    }
+    var rightHalf: CGRect {
+        return CGRect(x: midX, y: minY, width: width/2, height: height)
+    }
+    func inset(by size: CGSize) -> CGRect {
+        return insetBy(dx: size.width, dy: size.height)
+    }
+    func sized(to size: CGSize) -> CGRect {
+        return CGRect(origin: origin, size: size)
+    }
+    func zoom(by scale: CGFloat) -> CGRect {
+        let newWidth = width * scale
+        let newHeight = height * scale
+        return insetBy(dx: (width - newWidth) / 2, dy: (height - newHeight) / 2)
+    }
+}
+
+extension CGPoint {
+    func offsetBy(dx: CGFloat, dy: CGFloat) -> CGPoint {
+        return CGPoint(x: x+dx, y: y+dy)
+    }
+}
